f32769c04f345d69bc2b65c56a30629122407101
Mateusz Sikora <ms1qaz@gmail.com>
2024-12-31 13:11:36 +0100

    mul upper test program

--- bin/wrap.js
+++ bin/wrap.js
@@ -2,10 +2,10 @@
 
 // wrap input as program
 
-import { wrapAsProgram } from "../build/release.js";
-
-const program = wrapAsProgram(new Uint8Array([
-
+import {fuzz} from './fuzz.js'
+    
+const program = fuzz(new Uint8Array([
+    20,7,0, 0, 0, 0, 255, 255, 255, 255,20,8,0, 0, 0, 0, 1, 0, 0, 0,193,135,9,194,135,10,195,135,11
 ]));
 
 console.log(program);
