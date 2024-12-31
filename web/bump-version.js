02130f94776346566b5d798fcf6bbf3e40d3984b
Tomasz Drwięga <tomusdrw@gmail.com>
2024-12-31 18:07:13 +0100

    Add version.

---
+++ web/bump-version.js
@@ -0,0 +1,10 @@
+import fs from 'node:fs';
+
+const META_FILE = './web/pvm-metadata.json';
+const metadata = JSON.parse(fs.readFileSync(META_FILE));
+const packageJson = JSON.parse(fs.readFileSync('./package.json'));
+const hash = process.argv[2] || 'xxxxxx';
+
+metadata.version = `${packageJson.version}-${hash}`;
+
+fs.writeFileSync(META_FILE, JSON.stringify(metadata, null, 2));
