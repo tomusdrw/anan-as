1384846f19019af3250b1b16a35c273607314cca
HeyImStas <beebx4f@protonmail.com>
2025-09-23 14:04:33 +0200

    refactor

--- assembly/api.ts
+++ assembly/api.ts
@@ -9,8 +9,8 @@ import { NO_OF_REGISTERS, REG_SIZE_BYTES, Registers } from "./registers";
 
 let interpreter: Interpreter | null = null;
 
-export function resetJAM(program: u8[], pc: number, gas: Gas): void {
-  const p = decodeSpi(liftBytes(program), new Uint8Array(0));
+export function resetJAM(program: u8[], pc: number, gas: Gas, args: u8[] = []): void {
+  const p = decodeSpi(liftBytes(program), liftBytes(args));
   const int = new Interpreter(p, p.registers, p.memory);
   int.nextPc = <u32>pc;
   int.gas.set(gas);
