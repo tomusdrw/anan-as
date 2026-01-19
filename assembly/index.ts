3802bd0185989ca93acef2955c128bb540829c54
Tomasz Drwięga <tomusdrw@gmail.com>
2026-01-19 17:26:48 +0100

    Add WASM-PVM wrapper

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,4 +1,5 @@
 export * from "./api-debugger";
 export { getAssembly } from "./api-internal";
 export * from "./api-utils";
+export { main, result_len, result_ptr } from "./main-wrapper";
 export { wrapAsProgram } from "./program-build";
